3eacef9079d8e8a7ad32d9e45e0921b91d934882
k-mazats <k.mazats@gmail.com>
2020-10-31 05:37:22 +0100

    des trucs ...

--- commands.js
+++ commands.js
@@ -3,20 +3,21 @@ export default function commands(input) {
 	let splitSize = splitInput.length;
 	let message = "";
 	switch (splitInput[0]) {
-		case "?":
-			message = `Bienvenue sur BrunOS, si vous voulez un cours va falloir demander à Michel`;
+		case "-?":
+			message = `Si vous voulez un cours va falloir demander à Michel.`;
 			break;
 		case "quote":
-            message = `"quote" attends un argument.`;
+			message = `"quote" attends un argument.`;
 			switch (splitInput[1]) {
 				case "string":
 					message = `On appelle ça une string, désolé les filles !`;
 					break;
 				case "js":
 					message = `Le js c'est pas fait pour faire de l'objet !`;
-                    break;
-                    
+					break;
 			}
+		default:
+			message= `"${input}" n'est pas une commande valide. -? pour plus d'informations.`
 	}
 	return message;
 }
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import commands from "./commands.js"; // gere la reponse aux commandes 
+import commands from "./commands.js"; // gere la reponse aux commandes
 
 document.addEventListener("DOMContentLoaded", function () {
 	let terminalInput = document.getElementById("input");
@@ -6,13 +6,14 @@ document.addEventListener("DOMContentLoaded", function () {
 	let inputLeft = terminalInput.offsetLeft;
 	let inputTop = terminalInput.offsetTop;
 	let history = document.getElementById("terminalHistory");
+	let caretOffset = 0;
 
 	caretDiv.style.left = `${inputLeft}px`;
 	caretDiv.style.top = `${inputTop}px`;
 
 	function updateCaret() {
 		let strLength = terminalInput.value.length;
-		caretDiv.style.marginLeft = `${strLength + 1}ch`;
+		caretDiv.style.marginLeft = `${strLength + caretOffset + 1}ch`;
 		caretDiv.style.top = `${inputTop}px`;
 	}
 
@@ -41,7 +42,31 @@ document.addEventListener("DOMContentLoaded", function () {
 		if (event.code === "Enter") {
 			terminalSubmit();
 		}
-		fakeCaret();
+		if (event.code !== "Backspace" && input.value.length > 0) {
+			switch (event.code) {
+				case "ArrowLeft":
+					if (caretOffset - 1 >= 0 - input.value.length) {
+						caretOffset--;
+					}
+					break;
+				case "ArrowRight":
+					if (caretOffset + 1 <= 0) {
+						caretOffset++;
+					}
+					break;
+			}
+			if (caretOffset < 0 && caretDiv.classList.length === 0) {
+				caretDiv.classList += "caret-back";
+			} else if (caretOffset === 0) {
+				caretDiv.classList = [];
+			}
+			fakeCaret();
+		}
+	});
+	document.addEventListener("keyup", function (event) {
+		if (event.code === "Backspace") {
+			fakeCaret();
+		}
 	});
 
 	fakeCaret();
