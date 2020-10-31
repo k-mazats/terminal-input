c960766e24e169bb768f761d3cd034a3f43a8cf6
k-mazats <k.mazats@gmail.com>
2020-10-31 23:41:30 +0100

    up&down history mode

--- script.js
+++ script.js
@@ -42,10 +42,13 @@ document.addEventListener("DOMContentLoaded", function () {
 		"ControlRight",
 		"ShiftRight",
 	];
-	//"MetaLeft",
 
 	let tempInput = "";
-
+	let commandsHistory = [];
+	let commandsHistoryIndex = 0;
+	let browsing = false;
+	let inputMemory ="";
+	//FUNCTIONS
 	function updateCaret() {
 		let inputLeft = terminalInput.offsetLeft;
 		let inputTop = terminalInput.offsetTop;
@@ -128,10 +131,13 @@ document.addEventListener("DOMContentLoaded", function () {
 				<div>${reply[i]}</div>
 			</div>`;
 		}
+		commandsHistory.push(terminalInput.value);
 		terminalInput.value = "";
 		tempInput = "";
+		inputMemory="";
 		caretOffset = 0;
 	}
+
 	function updateInput(key) {
 		if (key === "Backspace") {
 			let inputArray = tempInput.split("");
@@ -173,10 +179,49 @@ document.addEventListener("DOMContentLoaded", function () {
 			}
 		}
 	}
+
+	function browseHistory(key){
+		
+		console.log(commandsHistoryIndex);
+		console.table(commandsHistory);
+		switch (key) {
+			case "ArrowUp":
+				if (commandsHistoryIndex === 0 && browsing === false) {
+					commandsHistoryIndex = commandsHistory.length - 1;
+					inputMemory = terminalInput.value;
+					terminalInput.value = commandsHistory[commandsHistoryIndex];
+				} else if (commandsHistoryIndex === 0) {
+					terminalInput.value = commandsHistory[commandsHistoryIndex];
+				} else {
+					commandsHistoryIndex--;
+					
+					terminalInput.value = commandsHistory[commandsHistoryIndex];
+				}
+				browsing = true;
+				break;
+			case "ArrowDown":
+				if (
+					commandsHistoryIndex !== 0 &&
+					browsing === true &&
+					commandsHistoryIndex !== commandsHistory.length -1
+				) {
+					commandsHistoryIndex++;
+					terminalInput.value = commandsHistory[commandsHistoryIndex];
+				} else if (
+					browsing === true
+				) {
+					commandsHistoryIndex = 0;
+					terminalInput.value = inputMemory;
+					browsing= false;
+				} break;
+		}
+	}
+
+	//EVENTS LISTENERS
 	document.addEventListener("click", function () {
 		fakeCaret();
 	});
-	document.addEventListener("keydown", function (event) {
+	document.addEventListener("keyup", function (event) {
 		if (
 			importantKeys.indexOf(event.code) === -1 &&
 			importantKeys.indexOf(event.key) === -1
@@ -191,6 +236,12 @@ document.addEventListener("DOMContentLoaded", function () {
 					break;
 				case "ArrowRight":
 					browseCaret(event.code);
+					break;
+				case "ArrowUp":
+					browseHistory(event.key);
+					break;
+				case "ArrowDown":
+					browseHistory(event.key);
 					break;
 				case "Delete":
 					updateInput(event.key);
