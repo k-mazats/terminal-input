fd5de848b833acace53dc42138f12c188bb8132c
k-mazats <k.mazats@gmail.com>
2020-10-31 12:40:27 +0100

    fix quote.js

--- commands.js
+++ commands.js
@@ -1,26 +1,16 @@
 import helper from "./commands/helper.js";
+import quote from "./commands/quote.js"
+
 export default function commands(input) {
 	let splitInput = input.split(" ");
-	let splitSize = splitInput.length;
 	let message = "";
 	switch (splitInput[0]) {
 		case "-?":
 			message = helper();
 			break;
 		case "quote":
-			if (splitInput.length > 1) {
-				switch (splitInput[1]) {
-					case "string":
-						message = `On appelle ça une string, désolé les filles !`;
-						break;
-					case "js":
-						message = `Le js c'est pas fait pour faire de l'objet !`;
-						break;
-				}
-			}else{
-				message = `"quote" attends un argument. "quote -?" pour plus d'informations.`;
-				break;
-			}
+			message = quote(splitInput);
+			break;
 		default:
 			message = `"${input}" n'est pas une commande valide. -? pour plus d'informations.`;
 	}
---
+++ commands/quote.js
@@ -0,0 +1,12 @@
+export default function quote(splitInput) {
+	if (splitInput.length > 1) {
+		switch (splitInput[1]) {
+			case "string":
+				return `On appelle ça une string, désolé les filles !`;
+			case "js":
+				return `Le js c'est pas fait pour faire de l'objet !`;
+		}
+	} else {
+		return `"quote" attends un argument. "quote -?" pour plus d'informations.`;
+	}
+}
